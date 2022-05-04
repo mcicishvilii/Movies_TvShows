bd41691a034734deb1497d0842ece64542af361d
mcicishvilii <83747561+mcicishvilii@users.noreply.github.com>
2022-05-04 11:22:24 +0400

    სერიალებშიც და ფილმებშიც შესაბამისად აჩვენებს სახელებს და ყველა პარამეტრს. შემდეგი ნაბიჯია დეტაილს სქრინის გაკეთება

--- app/src/main/java/com/example/movies_tvshows/MoviesAdapter.kt
+++ app/src/main/java/com/example/movies_tvshows/MoviesAdapter.kt
@@ -4,14 +4,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movies_tvshows.Models.MovieModels.MovieItem
-import com.example.movies_tvshows.databinding.LayoutMovieTvShowItemBinding
+import com.example.movies_tvshows.databinding.LayoutMovieItemBinding
+
 
 class MoviesAdapter(val movieList: MutableList<MovieItem>) : RecyclerView.Adapter<MoviesViewHolder>() {
     private lateinit var itemClickListener: (MovieItem, Int) -> Unit
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
         val binding =
-            LayoutMovieTvShowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            LayoutMovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MoviesViewHolder(binding)
     }
 
@@ -27,9 +28,10 @@ class MoviesAdapter(val movieList: MutableList<MovieItem>) : RecyclerView.Adapte
 
     override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
         val movie = movieList[position]
-        holder.binding.tvMovieName.text = movie.originalTitle
+        holder.binding.tvMovieName.text = "title: \n${movie.originalTitle}"
         holder.binding.tvRating.text = movie.voteAverage.toString()
-        holder.binding.tvReleaseDate.text = movie.releaseDate.toString()
+        holder.binding.tvReleaseDate.text = "release date: \n${movie.releaseDate.toString()}"
+        holder.binding.tvAverageRating.text = "total votes: \n${movie.voteCount.toString()}"
 
         holder.binding.tvMovieName.setOnClickListener {
             itemClickListener.invoke(movie,position)
@@ -41,7 +43,7 @@ class MoviesAdapter(val movieList: MutableList<MovieItem>) : RecyclerView.Adapte
     }
 }
 
-class MoviesViewHolder(val binding: LayoutMovieTvShowItemBinding) :
+class MoviesViewHolder(val binding: LayoutMovieItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
 }
